739358f87312c7b49040e0a476c537d5d1ce80f6
Hiten Samalia <30778907+hitensam@users.noreply.github.com>
2022-08-10 00:53:46 +0530

    loading animation added
    bg color yet not in html.

--- ProgrammingQuotes/index.js
+++ ProgrammingQuotes/index.js
@@ -1,15 +1,34 @@
 fetchData = async () =>
 {
-    h1.innerText = 'Loading!!'
-    p.innerText = 'Loading!!'
     DataRecvd = await axios.get('https://programming-quotes-api.herokuapp.com/Quotes/random')
+    const jsSelect = document.getElementById("jsSelect");
+            while (jsSelect.hasChildNodes()) {
+            jsSelect.removeChild(jsSelect.firstChild);
+            }
+    const selection = document.querySelector("#jsSelect")
+    const createElement = document.createElement('div')
+    createElement.innerHTML = `
+    <div class="pt-6" >
+        <div class = 'block'>
+            <h1 id="h1" class="title is-2"></h1>
+                <div class = 'block'>
+                    <div class="level-right">
+                        <div class="has-text-black-bis">
+                            <p id="p" class="subtitle is-4" ></p>
+                        </div>
+                    </div>
+                </div>
+        </div>   
+    </div>
+    `
+    selection.appendChild(createElement)
+    const h1 = document.querySelector('#h1')
+    const p = document.querySelector('#p')
     h1.innerText =  `${DataRecvd.data.en}`
     p.innerText = ` - ${DataRecvd.data.author}`
 }
 
-const h1 = document.querySelector('#h1')
-const p = document.querySelector('#p')
 fetchData().catch(() =>{
-    h1.innerText = 'Unable to Load'
-    p.innerText = 'please check your internet!!'
+   const loadingText = document.querySelector("#loading-text")
+   loadingText.textContent = "unable to connect"
 })
